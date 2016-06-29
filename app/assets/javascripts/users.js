bc125b879ce36f0e1be83df926c37efc2a639e4b
matthewbillienyc <matthewbillienyc@gmail.com>
2016-06-29 13:55:12 -0400

    ajax tips commented in

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,6 @@
+$(function() {
+  // put a listener on your form on submit or on your submit button on click (give it some unique id)
+  // preventDefault action
+  // capture form params and send them with ajax call to your controller
+  // on success, update the page with jquery somewhere showing the new user was posted successfully
+});
