f15874f0109c198d42ecefe7544d62d845ddcd58
matthewbillienyc <matthewbillienyc@gmail.com>
2016-07-05 12:28:28 -0400

    took out typo

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,7 +15,7 @@ class UsersControllerTest < ActionController::TestCase
         stub_request(:get, %r{.*\/accounts})
           .to_return(status: 200, body: account.to_json, headers: {})
         stub_request(:get, %r{.*\/users})
-          .to_return(status: 200, body: user.to_jsonyou , headers: {})
+          .to_return(status: 200, body: user.to_json , headers: {})
 
         #act
         get :show, id: 1
