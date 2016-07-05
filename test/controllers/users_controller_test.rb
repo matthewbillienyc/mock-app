5bcde46b61b2aab41cf2ea4080641e0acae2f4ac
HarrisonLavin <harrison.lavin@gmail.com>
2016-07-05 15:01:34 -0400

    stubs out all CRUD actions (minus destroy) in user_controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,6 +8,7 @@ class UsersControllerTest < ActionController::TestCase
 
   describe 'UserTest' do
     describe 'show' do
+
       it 'should display the name of requested user' do
         #arrange
         user = { first_name: 'Bob', last_name: 'Franco' }
@@ -37,6 +38,7 @@ class UsersControllerTest < ActionController::TestCase
     end
 
     describe 'index' do
+
       it 'should return all users' do
         users = [ { first_name: 'Bob', last_name: 'Franco' }, { first_name: 'Diane', last_name: 'Sawyer' }]
         account = { employer: 'employer', account_number: '123', organization: { name: 'org', state: 'ny' } }
@@ -52,9 +54,11 @@ class UsersControllerTest < ActionController::TestCase
         assert_response 200
         assert_template :index
       end
+
     end
 
     describe 'new' do
+
       it 'displays the form for a new user' do
         user = {first_name: "Frames", last_name: "Janco"}
         stub_request(:get, %r{.*\/accounts})
@@ -67,9 +71,11 @@ class UsersControllerTest < ActionController::TestCase
         assert_response 200
         assert_template :new
       end
+
     end
 
     describe 'create' do
+
       it 'posts data to the API' do
         user = {first_name: "Frames", last_name: "Janco"}
         stub_request(:post, %r{.*\/accounts})
@@ -77,22 +83,43 @@ class UsersControllerTest < ActionController::TestCase
         stub_request(:post, %r{.*\/users})
           .to_return(status: 200, body: " ", headers: {})
 
-        post :create
+        post :create, name: user
 
         assert_response 200
       end
     end
 
     describe 'edit' do
-      it 'displays the edit form for a new user' do
-        new_user_info = {first_name: "Frames", last_name: "Janco"}
-      end
 
+      it 'displays the edit form for a new user' do
+        user = { first_name: 'Bob', last_name: 'Franco' }
+        account = { employer: 'employer', account_number: '123', organization: { name: 'org', state: 'ny' } }
+        stub_request(:get, %r{.*\/accounts/\d})
+          .to_return(status: 200, body: account.to_json, headers: {})
+        stub_request(:get, %r{.*\/users/\d})
+          .to_return(status: 200, body: user.to_json, headers: {})
+
+        get :edit, id: 1
+
+        assert_response 200
+        assert_template :edit
+      end
     end
 
     describe 'update' do
+      it 'changes the listed data for a user' do
+        user = { first_name: 'Bob', last_name: 'Franco' }
+        account = { employer: 'employer', account_number: '123', organization: { name: 'org', state: 'ny' } }
+        stub_request(:put, "http://localhost:8080/mockapi/users/1?first_name=Bob&last_name=Franco").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => "", :headers => {})
 
+        post :update, id: 1, name: user
+
+        assert_response 200
+      end
     end
+
 
   end
 end
