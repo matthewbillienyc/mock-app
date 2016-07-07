38e8a8f76c9de4f253f0b19fc62dd5336b5c0524
HarrisonLavin <harrison.lavin@gmail.com>
2016-07-07 08:40:59 -0400

    more tests for cases

--- features/cases.feature
+++ features/cases.feature
@@ -5,3 +5,5 @@ Scenario: A user visits the cases index
   Then the page navigates to a list of all cases by description
 
 Scenario: A user visits a specific case
+  Given a user visiting the specific case
+  Then the page should show the description of the case and the user that filled it
--- features/step_definitions/cases_steps.rb
+++ features/step_definitions/cases_steps.rb
@@ -13,3 +13,11 @@ end
 #========================================#
 #     A user visits a specific case      #
 #========================================#
+Given(/^a user visiting the specific case$/) do
+  visit show_case_path(1)
+end
+
+Then(/^the page should show the description of the case and the user that filled it$/) do
+  has_text?('A new and interesting turn of events')
+  has_link?('Jarlett Scohanson')
+end
